9b29730964d07de88c99eda4c39cd9b4ed3adb7d
Qinjun Jiang <qjiang47@wisc.edu>
2018-10-13 15:50:51 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Qinjun Jiang on 10/13/18.
+//  Copyright © 2018 Qinjun Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
